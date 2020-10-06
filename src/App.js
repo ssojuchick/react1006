797a2c119f7f19aad34dd1e7ce57ec3f11183ed2
ssojuchick <ssojuchick@naver.com>
2020-10-06 18:31:17 +0900

    Real First!

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React?
         </a>
+        Hello, World!! I'm ssumthingood!!
       </header>
     </div>
   );
